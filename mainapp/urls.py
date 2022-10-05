4e910f89e9a8c9d90b7f762385222f4f01e4a6ea
santatra <razafindratsimba34@gmail.com>
2022-10-05 09:14:42 +0000

    2nd commit

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('login', views.login, name='login'),
+    path('login', views.login_page, name='login'),
+    path('logout', views.logout_page, name='logout'),
     path('add_user', views.add_user, name='add_user'),
     path('users', views.accepted_user, name='users'),
     path('today', views.today, name='today'),
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,17 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from mainapp.models import AttendanceRecord, User
-from .forms import AddUserForm
-from django.contrib.auth import authenticate, login, logout
+from .forms import AddUserForm  # forms
+from django.contrib.auth import authenticate, login, logout  # user login and logout
+
+# for flash messages
+from django.contrib import messages
+
+# We put decorator on every view that we want to be restricted when the user is logged out
+from django.contrib.auth.decorators import login_required
 
 
-def login(request):
-    if request.method == 'POST':
-        request.POST.get('username')
-        request.POST.get('password')
-    context = {}
-    return render(request, 'login.html', context)
+def login_page(request):
+    # Atao login_page satria login efa method Django
+
+    if request.user.is_authenticated:  # If already logged in, login page in not available anymore
+        return redirect('homepage')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'Login successfully !')
+                return redirect('homepage')
+            else:
+                messages.error(request, 'Username or password incorrect !')
+
+        context = {}
+        return render(request, 'login.html', context)
 
 
+def logout_page(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required(login_url='login')
 # View: index page
 def homepage(request):
 
@@ -20,6 +47,7 @@ def homepage(request):
     return render(request, 'index.html', context)
 
 
+@login_required(login_url='login')
 def add_user(request):
     form = AddUserForm()
     if request.method == 'POST':
@@ -30,6 +58,7 @@ def add_user(request):
     return render(request, 'add_user.html', context)
 
 
+@login_required(login_url='login')
 # View: today's attendance page
 def today(request):
     # attendance_instances_enum return an enumerated object
@@ -42,6 +71,7 @@ def today(request):
     return render(request, 'today.html', context=context)
 
 
+@login_required(login_url='login')
 def accepted_user(request):
     accepted_user_list = User.objects.filter(status='a')
     accepted_user_number = accepted_user_list.count()
@@ -52,6 +82,7 @@ def accepted_user(request):
     return render(request, 'accepted_user.html', context=context)
 
 
+@login_required(login_url='login')
 def full(request):
     # attendance_instances_enum return an enumerated object
     # We use it to have a new ID. It's not for the core database, just for display. View, not controller
